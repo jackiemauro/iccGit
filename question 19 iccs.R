14e260d9eef4cb3d7b9579550e064a0263b855e4
Jackie M <jacqueline.amauro@gmail.com>
2015-04-14 13:00:38 -0400

    Cleaned up names and created output files for question 19, but only for the regression analysis (not sum or average).

--- question 19 iccs.R
+++ question 19 iccs.R
@@ -118,53 +118,65 @@ for(type in unique(SID.type)){
 ################# with covariates ###############
 
 #SID level
-uncond.SID.cov <- lmer(pi.jk~1 + ed + marital + working + work.type + 
-                         age + + age2 + race + eth + children + income + 
-                         inc.ed + gender + victim + yrs.in.nbh + 
-                         (1|SID))
-df.SID.cov <- as.data.frame(VarCorr(uncond.SID.cov))
-icc.SID.cov <- df.SID.cov[1,4]/(df.SID.cov[1,4]+df.SID.cov[2,4]) #0.06316817
-
-# zip level
-lmer.zip.cov<-lmer(pi.jk~1 + ed + marital + working + work.type + 
-                     age + + age2 + race + eth + children + income + 
-                     inc.ed + gender + victim + yrs.in.nbh + 
-                     (1|zip))
-df.zip.cov <- as.data.frame(VarCorr(lmer.zip.cov))
-icc.zip.cov <- df.zip.cov[1,4]/(df.zip.cov[1,4]+df.zip.cov[2,4]) #0.00418688
-
-#look at combined level
-lmer.both.cov<-lmer(pi.jk~1 + ed + marital + working + work.type + 
+covs.SID.19 <- lmer(pi.jk~1 + ed + marital + working + work.type + 
                       age + + age2 + race + eth + children + income + 
                       inc.ed + gender + victim + yrs.in.nbh + 
-                      (1|SID) + (1|zip))
-df.both.cov <- as.data.frame(VarCorr(lmer.both.cov))
-icc.sid.both.cov <- df.both.cov[1,4]/(df.both.cov[1,4]+df.both.cov[2,4]+
-                                        df.both.cov[3,4]) #0.0631682
-icc.zip.both.cov <- df.both.cov[2,4]/(df.both.cov[1,4]+df.both.cov[2,4]+
-                                        df.both.cov[3,4]) #0
+                      (1|SID))
+df.covs.19.SID <- as.data.frame(VarCorr(covs.SID.19))
+icc.covs.67.SID <- df.covs.19.SID[1,4]/(df.covs.19.SID[1,4]+df.covs.19.SID[2,4]) #0.06316817
+
+# zip level
+covs.zip.19<-lmer(pi.jk~1 + ed + marital + working + work.type + 
+                    age + + age2 + race + eth + children + income + 
+                    inc.ed + gender + victim + yrs.in.nbh + 
+                    (1|zip))
+df.covs.19.SID <- as.data.frame(VarCorr(covs.zip.19))
+icc.covs.19.SID <- df.covs.19.SID[1,4]/(df.covs.19.SID[1,4]+df.covs.19.SID[2,4]) #0.00418688
+
+#look at combined level
+covs.both.19<-lmer(pi.jk~1 + ed + marital + working + work.type + 
+                     age + + age2 + race + eth + children + income + 
+                     inc.ed + gender + victim + yrs.in.nbh + 
+                     (1|SID) + (1|zip))
+df.covs.19.both <- as.data.frame(VarCorr(covs.both.19))
+icc.covs.both.19.sid <- df.covs.19.both[1,4]/(df.covs.19.both[1,4]+df.covs.19.both[2,4]+
+                                                df.covs.19.both[3,4]) #0.0631682
+icc.covs.both.19.zip <- df.covs.19.both[2,4]/(df.covs.19.both[1,4]+df.covs.19.both[2,4]+
+                                                df.covs.19.both[3,4]) #0
 
 
 #by sid type
 
-icc.SID.lev.covs = NULL
-icc.zip.lev.covs = NULL
-df.SID.lev.covs = NULL
+icc.covs.19.SID.lev = NULL
+icc.covs.19.zip.lev = NULL
+df.19.SID.lev.covs = NULL
 for(type in unique(SID.type)){
   #SID level
-  SID.lev.covs <- lmer(pi.jk~1 + ed + marital + working + work.type + 
-                         age + + age2 + race + eth + children + income + 
-                         inc.ed + gender + victim + yrs.in.nbh + 
-                         (1|SID) + (1|zip), data = for.icc.19[SID.type == type,])
-  summary(SID.lev.covs)
-  df.SID.covs <- as.data.frame(VarCorr(SID.lev.covs))
-  icc.SID.lev.covs[type] <- df.SID.covs[1,4]/(df.SID.covs[1,4]+df.SID.covs[2,4]+df.SID.covs[3,4]) 
-  icc.zip.lev.covs[type] <- df.SID.covs[2,4]/(df.SID.covs[1,4]+df.SID.covs[2,4]+df.SID.covs[3,4])
-  df.SID.lev.covs <- c(df.SID.lev.covs, df.covs[1,4])
+  covs.both.19.lev <- lmer(pi.jk~1 + ed + marital + working + work.type + 
+                             age + + age2 + race + eth + children + income + 
+                             inc.ed + gender + victim + yrs.in.nbh + 
+                             (1|SID) + (1|zip), data = for.icc.19[SID.type == type,])
+  summary(covs.both.19.lev)
+  df.covs.19.lev <- as.data.frame(VarCorr(covs.both.19.lev))
+  icc.covs.19.SID.lev[type] <- df.covs.19.lev[1,4]/(df.covs.19.lev[1,4]+df.covs.19.lev[2,4]+df.covs.19.lev[3,4]) 
+  icc.covs.19.zip.lev[type] <- df.covs.19.lev[2,4]/(df.covs.19.lev[1,4]+df.covs.19.lev[2,4]+df.covs.19.lev[3,4])
+  df.19.SID.lev.covs <- c(df.19.SID.lev.covs, df.covs.19.lev[1,4])
 }
 
-write.csv(cbind(summary(SID.lev.covs)$coef[,1], summary(SID.lev.covs)$coef[,3]),
+write.csv(cbind(summary(covs.both.19.lev)$coef[,1], summary(covs.both.19.lev)$coef[,3]),
           file = "q19 coefs.csv")
+
+var.icc.19 = data.frame(vars = df.19.SID.lev.covs, iccs = icc.covs.19.SID.lev)
+overalls.19 = data.frame(vars = c(df.covs.19.both[2,4], df.covs.19.both[1,4]),
+                         iccs = c(icc.covs.both.19.zip, icc.covs.both.19.sid))
+output.unordered = rbind(overalls.19, var.icc.19)
+output = rbind(output.unordered[1:2,],
+               output.unordered[which(labels(output.unordered)[[1]] == "Cold Spot"),],
+               output.unordered[which(labels(output.unordered)[[1]] == "Cool Spot"),],
+               output.unordered[which(labels(output.unordered)[[1]] == "Violent Spot"),],
+               output.unordered[which(labels(output.unordered)[[1]] == "Drug Spot"),],
+               output.unordered[which(labels(output.unordered)[[1]] == "Combined"),])
+write.csv(output, file = "q19 vars.csv")
 
 
 Spot = factor(names(icc.SID.lev.covs), 
@@ -180,11 +192,12 @@ ggplot(mm, aes(x = factor(Spot), y = test)) +
   geom_hline(aes(yintercept = icc.sid.both.cov), col = "red") + 
   geom_hline(aes(yintercept = icc.zip.both.cov), col = "blue") +
   ylab("SID level ICC") + 
-  ggtitle("ICC for combined ZIP and SID without covariates")
+  ggtitle("ICC for combined ZIP and SID with covariates q19")
 
 
 detach(for.icc.19)
 attach(for.icc.avg)
+
 
 ########################### get ICCs for averages ##############
 
@@ -317,6 +330,10 @@ for(type in unique(SID.type)){
 
 # Cool Spot Violent Spot    Drug Spot     Combined    Cold Spot 
 # 0.04703118   0.09485493   0.06552409   0.04193855   0.00000000 
+
+
+
+
 
 require(ggplot2)
 require(plyr)
